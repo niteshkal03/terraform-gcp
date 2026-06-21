a5826858f48c3e88b15235d4fb858ee08b0c842d
niteshkal03 <niteshkalangada888@gmail.com>
2026-06-21 04:33:06 +0000

    New chnages added in Index.html file

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  default = "project-93292acb-1b2a-49eb-8cd"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "service_name" {
+  default = "demo-app"
+}
